a1f6c6ca667a55f5524892785a927bf56b951c95
fariz rafi <farizrafi2001@gmail.com>
2026-05-23 15:31:56 +0530

    final working home route

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.staticfiles import StaticFiles
 
 from app.database import engine, Base
 
@@ -9,7 +10,6 @@ from app.models.timeline_model import TimelineEvent
 from app.models.hearing_model import Hearing
 from app.models.notification_model import Notification
 from app.models.document_model import Document
-from fastapi.staticfiles import StaticFiles
 
 # Import Routers
 from app.routes import notification_routes
@@ -35,14 +35,14 @@ from app.routes.page_routes import (
 
 from app.routes import timeline_routes
 from app.services.auth_service import verify_token
-from app.routes.api_page_routes import router as api_page_router
-
 
 # Create Database Tables
 Base.metadata.create_all(bind=engine)
 
-
+# Create App
 app = FastAPI()
+
+# Static Folder
 app.mount(
     "/static",
     StaticFiles(directory="app/static"),
@@ -69,10 +69,8 @@ app.include_router(dashboard_router)
 app.include_router(websocket_router)
 
 app.include_router(page_router)
+
 app.include_router(timeline_routes.router)
-
-#app.include_router(api_page_router)
-
 
 # Protected Test Route
 @app.get("/protected")
@@ -81,6 +79,14 @@ def protected_route(
 ):
 
     return {
-       "message": "Protected route accessed",
+        "message": "Protected route accessed",
         "user": user_data
+    }
+
+# Home Route
+@app.get("/")
+def home():
+
+    return {
+        "message": "Legal Case Management API Running Successfully"
     }
