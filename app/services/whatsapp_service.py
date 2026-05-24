803d9ebcedee6f95cff08f5f117e9f3d332ca2b8
fariz rafi <farizrafi2001@gmail.com>
2026-05-24 15:38:57 +0530

    twilio debug logs

--- app/services/whatsapp_service.py
+++ app/services/whatsapp_service.py
@@ -2,12 +2,29 @@ import os
 
 from twilio.rest import Client
 
-TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
-TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+
+
+# =========================
+# ENV VARIABLES
+# =========================
+
+TWILIO_ACCOUNT_SID = os.getenv(
+    "TWILIO_ACCOUNT_SID"
+)
+
+TWILIO_AUTH_TOKEN = os.getenv(
+    "TWILIO_AUTH_TOKEN"
+)
 
 TWILIO_WHATSAPP_NUMBER = os.getenv(
     "TWILIO_WHATSAPP_NUMBER"
 )
+
+
+
+# =========================
+# TWILIO CLIENT
+# =========================
 
 client = Client(
     TWILIO_ACCOUNT_SID,
@@ -15,12 +32,45 @@ client = Client(
 )
 
 
+
+# =========================
+# SEND WHATSAPP MESSAGE
+# =========================
+
 def send_whatsapp_message(
     to_number: str,
     message: str
 ):
 
     try:
+
+        print("\n=========================")
+        print("TWILIO WHATSAPP STARTED")
+        print("=========================")
+
+        print(
+            "ACCOUNT SID:",
+            TWILIO_ACCOUNT_SID
+        )
+
+        print(
+            "FROM NUMBER:",
+            TWILIO_WHATSAPP_NUMBER
+        )
+
+        print(
+            "TO NUMBER:",
+            f"whatsapp:{to_number}"
+        )
+
+        print(
+            "MESSAGE:",
+            message
+        )
+
+
+
+        # SEND MESSAGE
 
         response = client.messages.create(
 
@@ -31,14 +81,41 @@ def send_whatsapp_message(
             to=f"whatsapp:{to_number}"
         )
 
+
+
+        print("=========================")
+        print("WHATSAPP SENT SUCCESS")
+        print("=========================")
+
+        print(
+            "MESSAGE SID:",
+            response.sid
+        )
+
+
+
         return {
+
             "success": True,
+
             "sid": response.sid
         }
 
+
+
     except Exception as e:
 
+        print("=========================")
+        print("TWILIO ERROR")
+        print("=========================")
+
+        print(str(e))
+
+
+
         return {
+
             "success": False,
+
             "error": str(e)
         }
